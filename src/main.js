dfeb927c4557e490494ade949f3ba76a1272f00d
unknown <1165054597@qq.com>
2020-04-27 22:06:53 +0800

    first

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+// import 'lib-flexible/flexible.js'
+import 'amfe-flexible'
+import './css/reset.css'
+import './less/base.less'
+import axios from "axios";
+Vue.config.productionTip = false
+Vue.prototype.$axios=axios;
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
---
+++ vue.config.js
@@ -0,0 +1,58 @@
+const debug = process.env.NODE_ENV !== 'production'
+const path = require('path')
+module.exports = {
+    outputDir: 'dist', // 构建输出目录
+    assetsDir: 'assets', // 静态资源目录 (js, css, img, fonts)
+    lintOnSave: false, // 是否开启eslint保存检测，有效值：ture | false | 'error'
+    runtimeCompiler: true, // 运行时版本是否需要编译
+    transpileDependencies: [], // 默认babel-loader忽略mode_modules，这里可增加例外的依赖包名
+    productionSourceMap: true, // 是否在构建生产包时生成 sourceMap 文件，false将提高构建速度
+    configureWebpack: config => { // webpack配置，值位对象时会合并配置，为方法时会改写配置
+    },
+    chainWebpack: config => { // webpack链接API，用于生成和修改webapck配置，https://github.com/vuejs/vue-cli/blob/dev/docs/webpack.md
+        if (debug) {
+            // 本地开发配置
+        } else {
+            // 生产开发配置
+        }
+    },
+    parallel: require('os').cpus().length > 1, // 构建时开启多进程处理babel编译
+    pluginOptions: { // 第三方插件配置
+        'style-resources-loader': {
+          preProcessor: 'less',
+          patterns: [path.resolve(__dirname, "src/less/base.less")] // 引入全局样式变量
+        }
+    },
+    pwa: { // 单页插件相关配置 https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa
+    },
+    css: {
+        loaderOptions: {
+            postcss: {
+                // 这是rem适配的配置  注意： remUnit在这里要根据lib-flexible的规则来配制，如果您的设计稿是750px的，用75就刚刚好。
+                plugins: [
+                    require("postcss-px2rem")({
+                        remUnit: 75
+                    })
+                ]
+            }
+        }
+    },
+    devServer: {
+        open: true,
+        host: 'localhost',
+        port: 8080,
+        https: false,
+        hotOnly: false,
+        proxy: { // 配置跨域
+            '/domain': {
+                target: 'https://rapapi.renqilai.com/',
+                ws: true,
+                changOrigin: true,
+                pathRewrite: {
+                    '^/domain': ''
+                }
+            }
+        },
+        before: app => { }
+    }
+}
